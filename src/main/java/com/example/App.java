533524cdedb3e7a6067a876d10cd6d0c7fec1b1b
SnehaDevopsPractice <snehaprasad989@gmail.com>
2026-03-15 15:17:46 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,6 +3,7 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to My Simple Java Application!");
+        System.out.println("Running!");
     }
 }
 
